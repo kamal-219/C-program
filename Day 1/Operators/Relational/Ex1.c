7bc56ad86f9f52d65741a19a8bd485a43fb05c04
kamal-219 <144412001+kamal-219@users.noreply.github.com>
2025-05-14 10:16:13 +0530

    Create Ex1.c

---
+++ Day 1/Operators/Relational/Ex1.c
@@ -0,0 +1,18 @@
+//compare two number and print larger
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    printf("enter the a value");
+    scanf("%d",&a);
+    printf("enter the b value");
+    scanf("%d",&b);
+    if(a>b){
+        printf("a is larger");
+    }
+    else{
+        printf("b is larger");
+    }
+    return 0;
+   
+}
