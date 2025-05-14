6f06dd6d338bd9d940e4e6b4b5f614274ee49816
kamal-219 <144412001+kamal-219@users.noreply.github.com>
2025-05-14 09:45:57 +0530

    Create ex1.c

---
+++ Day 1/Operators/Arithmetic/ex1.c
@@ -0,0 +1,19 @@
+  // Arithmetic operations of two numbers
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    printf("enter the a value");
+    scanf("%d",&a);
+    printf("enter the b value");
+    scanf("%d",&b);
+    
+    
+    printf("addition %d\n",a+b);
+    printf("sub %d\n",a-b);
+    printf("multiply %d\n",a*b);
+    printf("division r %d\n",a/b);
+    printf("division q %d\n",a%b);
+    
+    return 0;
+}
