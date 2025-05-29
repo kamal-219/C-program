77925d4d07b1632fda9d9f0f6d76d358da601a64
kamal-219 <144412001+kamal-219@users.noreply.github.com>
2025-05-29 10:00:17 +0530

    Create sign Vs Unsign.c

---
+++ Assignments/Assignment 2/sign Vs Unsign.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Declare signed and unsigned integers
+    signed int signedNum = -10;
+    unsigned int unsignedNum = -10;
+
+    // Display values
+    printf("Signed Integer Value   : %d\n", signedNum);
+    printf("Unsigned Integer Value : %u\n", unsignedNum);
+
+    return 0;
+}
