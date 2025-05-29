fd04ba87f9ab3bd3ebea2d58121c957ba5ebd28f
kamal-219 <144412001+kamal-219@users.noreply.github.com>
2025-05-29 13:46:04 +0530

    Create User input.c

---
+++ Assignments/Assignment 2/User input.c
@@ -0,0 +1,15 @@
+//program that accepts user input for three variables and prints their values.
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter tha value of a--\n");
+    scanf("%d",&a);
+    printf("Enter tha value of b--\n");
+    scanf("%d",&b);
+    printf("Enter tha value of c--\n");
+    scanf("%d",&c);
+    printf("The Value of a,b,c are--a=%d\n,b=%d\n,c=%d",a,b,c);
+
+    return 0;
+}
