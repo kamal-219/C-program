fd7460da52ed3a43ac437ea44a1d7757df67041d
kamal-219 <144412001+kamal-219@users.noreply.github.com>
2025-05-29 10:26:12 +0530

    Create Asci addition.c

---
+++ Assignments/Assignment 2/Asci addition.c
@@ -0,0 +1,12 @@
+//Implement a program that accepts two characters and performs addition on their ASCII values.
+#include <stdio.h>  
+int main(){
+    char a='F';
+    char b='G';
+    printf("%d\n",a);
+    printf("%d\n",b);
+    int c=a+b;
+    printf("%d",c);
+    return 0;
+}
+  
