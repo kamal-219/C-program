c047e5d8ca2c7125bb98ee4906a25df66b5cb191
kamal-219 <144412001+kamal-219@users.noreply.github.com>
2025-05-29 09:04:20 +0530

    Create sizeof.c

---
+++ Assignments/Assignment 1/sizeof.c
@@ -0,0 +1,11 @@
+// Size of operators
+#include <stdio.h>
+
+int main() {
+    printf("int size %lu \n",sizeof(int));
+    printf("float size %lu \n",sizeof(float));
+    printf("char size %lu \n",sizeof(char));
+    printf("Double size %lu \n",sizeof(double));
+    return 0;
+}
+  
