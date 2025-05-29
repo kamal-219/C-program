53ec145b37c88170dda881368774bfda5a94776b
kamal-219 <144412001+kamal-219@users.noreply.github.com>
2025-05-29 15:03:37 +0530

    Create Compound.c

---
+++ Assignments/Assignment 2/Compound.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    float r,CI,P,n;
+    double A;
+    printf("Enter the Priniciple amount--\n");
+    scanf("%f",&P);
+    printf("Enter the Rate of intrest in precentage--\n");
+    scanf("%f",&r);
+    printf("Enter the number of years--\n");
+    scanf("%f",&n);
+    A=P*(pow((1+r*0.01),n));
+    printf("Annual intrest is--%2lf\n",A);
+    CI=A-P;
+    printf("Compound intrest is--%2f",CI);
+    return 0;
+}
