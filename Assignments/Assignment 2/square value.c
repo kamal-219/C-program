4edcf098990220afbbe2123e763ca0acf5e9ab60
kamal-219 <144412001+kamal-219@users.noreply.github.com>
2025-05-29 11:50:26 +0530

    Create square value.c

---
+++ Assignments/Assignment 2/square value.c
@@ -0,0 +1,14 @@
+//program to calculate the square of a number using a variable and assign the result to another variable.
+#include <stdio.h>
+
+int main() {
+    int number = 7;       // Input number
+    int square;           // Variable to store the square
+
+    square = number * number;  // Calculate square
+
+    printf("Number: %d\n", number);
+    printf("Square: %d\n", square);
+
+    return 0;
+}
