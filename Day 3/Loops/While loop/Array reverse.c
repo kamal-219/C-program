27477a6a13a89196a0408d79848284e9793898d9
kamal-219 <144412001+kamal-219@users.noreply.github.com>
2025-05-30 10:18:38 +0530

    Create Array reverse.c

---
+++ Day 3/Loops/While loop/Array reverse.c
@@ -0,0 +1,13 @@
+// Array Reverse
+#include <stdio.h>
+
+int main() {
+    int i,a[4]={3,6,7,9};
+    printf("the reversed array--");
+    i=3;
+    while(i>=0){
+        printf("%d",a[i]);
+        i--;
+    }
+    return 0;
+}
