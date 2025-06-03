021807ea6e29ad07e90ad02beca4ee6d3dc12d31
kamal-219 <144412001+kamal-219@users.noreply.github.com>
2025-06-03 13:50:18 +0530

    Update Ex 4.c

--- Day 3/Bit Manuplation/Ex 4.c
+++ Day 3/Bit Manuplation/Ex 4.c
@@ -1,17 +1,15 @@
-// Implement an efficient algorithm to find the only non-repeating number in an array.
 #include <stdio.h>
 
 int main() {
-    int i,j,a[] = {3, 6, 7, 6, 9, 2, 3},output=0;
-    int len=sizeof(a)/sizeof(a[0]);
-    for(i=0;i<len;i++){
-        for(j=i+1;j<len;j++){
-            output^=a[i];
-            if(output!=0){
-              printf("%d",a[i]);
-        }
-       
-       }
-    } 
+    int a[] = {4, 6, 5, 4, 9, 2, 6, 2, 5};  
+    int output = 0;
+    int len = sizeof(a) / sizeof(a[0]);
+
+    for (int i = 0; i < len; i++) {
+            output ^= a[i];
+    }
+
+    printf("The non-repeating number is: %d\n", output);
+
     return 0;
 }
